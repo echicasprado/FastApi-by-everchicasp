1797062323042b301318319c6f7c2a13cb09b29f
everchicas <everchicasp@outllook.com>
2024-05-22 09:19:42 -0600

    add main and router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routers import AreaRouter
 
+BASE_URL="info-projects"
+
 app = FastAPI(
     title="Info projects API",
     version="0.0.1"
--- app/routers/AreaRouter.py
+++ app/routers/AreaRouter.py
@@ -1,19 +1,44 @@
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from typing import List
-from ..models.area.shema import Area, AreaGet, AreaPost, AreaUpdate, AreaDelete
-
-DB_FAKE = [
-    Area(id=1,description="Area 1", available=True),
-    Area(id=2,description="Area 2", available=True),
-    Area(id=3,description="Area 3", available=True)
+from models.area.shema import Area, AreaGet, AreaPost, AreaUpdate, AreaDelete
+ 
+FAKE = [
+    Area(id=1, description="Area 1", available=True),
+    Area(id=2, description="Area 2", available=False),
+    Area(id=3, description="Area 3", available=True),
 ]
 
+
 router = APIRouter(
-    prefix= "/areas",
+    prefix="/areas",
     tags= ["Areas"]
 )
 
 @router.get("/", status_code=200)
-def area_home():
-    return HTMLResponse("<h1> Areas info projects</h1>")
+def areas_home():
+    return HTMLResponse("<h1>Areas info projects</h1>")
+
+@router.get("/get-areas", response_model=List[Area], status_code=200)
+def getAreas():
+    return FAKE
+
+@router.post("/create-area", response_model=AreaGet, status_code=201)
+def createNewArea(area:AreaPost) -> AreaGet:
+    newArea = Area(id=len(FAKE) + 1, description=area.description,available=True)
+    FAKE.append(newArea)
+    return newArea
+
+@router.patch("update-area",response_model=AreaGet, status_code=200)
+def updateArea(areaUpdate:AreaUpdate):
+    for area in FAKE:
+        if area.id == areaUpdate.id:
+            area.description = areaUpdate.description
+            return area
+        
+@router.delete("delete-area", status_code=200)
+def deleteArea(areaDelete: AreaDelete):
+    for area in FAKE:
+        if area.id == areaDelete.id:
+            area.available = False
+            return HTMLResponse("Operación exitosa")

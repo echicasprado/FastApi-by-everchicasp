6b0028f278329f46da8c5ad547ca77c794a6983c
everchicas <everchicasp@outllook.com>
2024-06-04 11:36:32 -0600

    update querys

--- app/modules/area/service.py
+++ app/modules/area/service.py
@@ -9,36 +9,33 @@ from .shema import Area, AreaCreate
 class AreaServices():
 
     def get_all_areas(db: Session) -> List[Area]:
-        areas = (
+        return (
             db.query(AreaModel)
             .options(joinedload(AreaModel.estado))
-            ).all()
-        return areas
+            .all()
+        )
     
     def get_available_areas(db:Session) -> List[Area]:
-        areas = (
+        return (
             db.query(AreaModel)
             .filter(AreaModel.id_estado == EstadoEnum.ACTIVO)
             .options(joinedload(AreaModel.estado))
-            ).all()
-        return areas
+            .all()
+        )
     
     def get_area_by_parameters(db:Session, skip:int, limit:int, sort_by:str, order:str)->List[AreaModel]:
         order_by_column = getattr(AreaModel,sort_by)
- 
-        query = (
+
+        return (
             db.query(AreaModel)
             .filter(AreaModel.id_estado == EstadoEnum.ACTIVO)
             .options(joinedload(AreaModel.estado))
-            )
-
-        if order == "desc":
-            query = query.order_by(desc(order_by_column))
-        else:
-            query = query.order_by(asc(order_by_column))
+            .order_by(desc(order_by_column) if order == "desc" else asc(order_by_column))
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
         
-        areas = query.offset(skip).limit(limit).all()
-        return areas
 
     def get_area_by_id(db:Session, id_area:int) -> Area:
         return db.query(AreaModel).filter_by(id_estado = EstadoEnum.ACTIVO, id_area = id_area).options(joinedload(AreaModel.estado)).first()
@@ -50,26 +47,23 @@ class AreaServices():
         db_area = AreaModel(descripcion = area.descripcion, id_estado = EstadoEnum.ACTIVO)
         db.add(db_area)
         db.commit()
-        db.refresh(db_area)
-        db_area = db.query(AreaModel).filter_by(id_area = db_area.id_area).options(joinedload(AreaModel.estado)).first()
-        return db_area
+        
+        return db.query(AreaModel).filter_by(id_area = db_area.id_area).options(joinedload(AreaModel.estado)).first()
     
     def update_area(db:Session, id_area:int, area:AreaCreate) -> Area:
         db_area = db.query(AreaModel).filter_by(id_estado = EstadoEnum.ACTIVO, id_area = id_area).options(joinedload(AreaModel.estado)).first()
 
-        if db_area != None:
+        if db_area:
             db_area.descripcion = area.descripcion
             db.commit()
-            db.refresh(db_area)
 
         return db_area
     
     def delete_area(db:Session, id_area:int) -> Area:
         db_area = db.query(AreaModel).filter_by(id_estado = EstadoEnum.ACTIVO, id_area = id_area).options(joinedload(AreaModel.estado)).first()
 
-        if db_area != None:
+        if db_area:
             db_area.id_estado = EstadoEnum.INACTIVO
             db.commit()
-            db.refresh(db_area)
 
         return db_area
